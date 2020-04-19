2c127211b449f4f1a3eaf0149bff2a8af3f73f40
Arch <zubr_artur@bk.ru>
2020-04-19 22:30:39 +0300

    added argument parser reworked parsing method

--- wallpaper_parser.py
+++ wallpaper_parser.py
@@ -1,44 +1,67 @@
+import argparse
+import re
 import requests
 from bs4 import BeautifulSoup
 
 
 class Parser:
-    SITE = "https://www.smashingmagazine.com/category/wallpapers"
+    SITE = "https://www.smashingmagazine.com"
+    ALL_WALLPAPERS = "https://www.smashingmagazine.com/category/wallpapers"
 
-    def parse(self, month, year, size):
+    def parse(self, month, year, resolution):
         page_url = self.format_url(month, year)
         page_content = self.get_url(page_url)
-        image_links = self.grab_all_links(size, page_content)
+        image_links = self.grab_all_links(resolution, page_content)
         self.save_files(image_links)
 
     def get_url(self, url):
         return requests.get(url)
 
     def format_url(self, month, year):
+        month = month.lower()
         month_list = ("january", "february", "march", "april", "may",
                       "june", "july", "august", "september", "october",
                       "november", "december")
-        url = f"{self.SITE}/{year}/{month_list.index(month)}/"
-        url += f"desktop-wallpaper-calendars-{month}-{year}/"
+        url = f"{self.SITE}/{year}/{month_list.index(month):02d}/" \
+              f"desktop-wallpaper-calendars-{month}-{year}/"
         return url
 
-    def grab_all_links(self, size, html):
+    def grab_all_links(self, resolution, html):
         links = {}
-        soup = BeautifulSoup(html, 'html.parser')
-        for ul in soup.find_all('ul'):
-            for li in ul.find_all('li'):
-                content = li.contents
-                if content[0] == 'without calendar: ':
-                    for a_tag in content:
-                        if a_tag.contents[0] == size:
-                            title = a_tag['title']
-                            link = a_tag['href']
-                            links['title'] = link
+        soup = BeautifulSoup(html.text, 'html.parser')
+        for li in soup.find_all(text=re.compile('without calendar: ')):
+            tag = li.parent.find(text=re.compile(resolution))
+            if tag:
+                tp = tag.parent
+                link = str(tag.parent['href'])
+                file_extension = link[-4:]
+                name = str(tag.parent['title']) + file_extension
+                links[name] = link
         return links
 
     def save_files(self, links):
-        for title, link in links.values():
-            title += '.png'
+        for title in links.keys():
             with open(title, 'wb') as f:
-                image = self.get_url(link)
+                image = self.get_url(links[title])
                 f.write(image.content)
+
+
+if __name__ == '__main__':
+    app_description = """Download wallpaper from 
+    https://www.smashingmagazine.com in current directory which published
+    at chosen month year with preferred resolution"""
+    parser = argparse.ArgumentParser(description=app_description)
+    parser.add_argument('month', help='name of month in English',
+                        choices=["january", "february", "march", "april",
+                                 "may", "june", "july", "august",
+                                 "september", "october", "november",
+                                 "december"])
+    parser.add_argument('year', type=int, help='year of publication')
+    parser.add_argument('resolution', type=str, help='image resolution',
+                        choices=['800x480', '1024x768', '1280x720',
+                                 '1280x1024', ' 1600x1050', '1400x1050',
+                                 '1600x1200', '1920x1200', '2560x1440',
+                                 '3475x4633'])
+    args = parser.parse_args()
+    img_parser = Parser()
+    img_parser.parse(month=args.month, year=args.year, resolution=args.resolution)
